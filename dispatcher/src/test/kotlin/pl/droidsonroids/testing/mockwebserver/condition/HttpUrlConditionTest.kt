af1650e5079e960b3003a2a06a05c0b28c15623a
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-07-17 02:19:28 +0200

    Httpurlcondition test (#8)
    * prepared for 1.0.1 development iteration
    
    * `HttpUrlCondition` tests added

---
+++ dispatcher/src/test/kotlin/pl/droidsonroids/testing/mockwebserver/condition/HttpUrlConditionTest.kt
@@ -0,0 +1,49 @@
+package pl.droidsonroids.testing.mockwebserver.condition
+
+import com.nhaarman.mockito_kotlin.argumentCaptor
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
+import okhttp3.HttpUrl
+import okhttp3.mockwebserver.RecordedRequest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.mockito.Mockito.CALLS_REAL_METHODS
+import java.net.InetAddress
+import java.net.ServerSocket
+import java.net.Socket
+
+class HttpUrlConditionTest {
+
+	@Test
+	fun `request without URL does not match`() {
+		val matchAllUrlCondition = object : HttpUrlCondition() {
+			override fun isUrlMatching(url: HttpUrl) = true
+			override fun compareTo(other: Condition) = 0
+		}
+		val request = RecordedRequest(null, null, null, 0, null, 0, null)
+
+		assertThat(matchAllUrlCondition.isRequestMatching(request))
+	}
+
+	@Test
+	fun `request with URL checked by isUrlMatching` () {
+		val captor = argumentCaptor<HttpUrl>()
+		val condition:HttpUrlCondition = mock(defaultAnswer = CALLS_REAL_METHODS)
+		whenever(condition.isUrlMatching(captor.capture())).thenReturn(true)
+
+		val serverSocket = ServerSocket(0)
+		serverSocket.use {
+			val socket = Socket(serverSocket.inetAddress, serverSocket.localPort)
+			socket.use {
+				val request = RecordedRequest("GET / HTTP/1.1", null, null, 0, null, 0, socket)
+				assertThat(condition.isRequestMatching(request)).isTrue()
+
+				val url = captor.firstValue
+				assertThat(url.scheme()).isEqualTo("http")
+				assertThat(url.host()).isEqualTo(socket.inetAddress.hostName)
+				assertThat(url.pathSize()).isEqualTo(1)
+			}
+		}
+	}
+
+}
