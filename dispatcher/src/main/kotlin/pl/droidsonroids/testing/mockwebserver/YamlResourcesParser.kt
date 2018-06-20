770ee9eb0594902fb7c653d580e2a8fa0d0d6b56
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2018-06-20 17:39:25 +0200

    Yaml sanitizer draft

--- dispatcher/src/main/kotlin/pl/droidsonroids/testing/mockwebserver/YamlResourcesParser.kt
+++ dispatcher/src/main/kotlin/pl/droidsonroids/testing/mockwebserver/YamlResourcesParser.kt
@@ -1,14 +1,17 @@
 package pl.droidsonroids.testing.mockwebserver
 
+import org.apache.commons.text.translate.JavaUnicodeEscaper
 import org.yaml.snakeyaml.Yaml
 
 internal class YamlResourcesParser : ResourcesParser {
+    private val escaper = JavaUnicodeEscaper.above(0xD800)
     private val parser = Yaml()
 
     override fun parseFrom(fileName: String): Fixture {
         val path = "fixtures/$fileName.yaml"
         val content = path.getResourceAsString()
-        val result = parser.loadAs(content, Fixture::class.java)
+        val escapedContent = escaper.translate(content)
+        val result = parser.loadAs(escapedContent, Fixture::class.java)
 
         if (!result.hasJsonBody()) {
             val bodyPath = "fixtures/${result.body}"
