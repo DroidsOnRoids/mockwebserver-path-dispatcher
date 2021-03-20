abdfb255fd776fa2b5feeec284ae82fd4e8824c4
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2021-03-20 12:22:20 +0100

    Remove senseless test Update changelog

--- dispatcher/src/test/kotlin/pl/droidsonroids/testing/mockwebserver/condition/HttpUrlConditionTest.kt
+++ dispatcher/src/test/kotlin/pl/droidsonroids/testing/mockwebserver/condition/HttpUrlConditionTest.kt
@@ -1,16 +1,11 @@
 package pl.droidsonroids.testing.mockwebserver.condition
 
-import com.nhaarman.mockito_kotlin.argumentCaptor
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
 import okhttp3.Headers
 import okhttp3.HttpUrl
 import okhttp3.mockwebserver.RecordedRequest
 import okio.Buffer
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import org.mockito.Mockito.CALLS_REAL_METHODS
-import java.net.ServerSocket
 import java.net.Socket
 
 class HttpUrlConditionTest {
@@ -23,28 +18,6 @@ class HttpUrlConditionTest {
 		}
 		val request = RecordedRequest("", Headers.headersOf(), emptyList(), 0, Buffer(), 0, Socket())
 
-		assertThat(matchAllUrlCondition.isRequestMatching(request))
+		assertThat(matchAllUrlCondition.isRequestMatching(request)).isFalse
 	}
-
-	@Test
-	fun `request with URL checked by isUrlMatching` () {
-		val captor = argumentCaptor<HttpUrl>()
-		val condition:HttpUrlCondition = mock(defaultAnswer = CALLS_REAL_METHODS)
-		whenever(condition.isUrlMatching(captor.capture())).thenReturn(true)
-
-		val serverSocket = ServerSocket(0)
-		serverSocket.use {
-			val socket = Socket(serverSocket.inetAddress, serverSocket.localPort)
-			socket.use {
-				val request = RecordedRequest("GET / HTTP/1.1", Headers.headersOf(), emptyList(), 0, Buffer(), 0, Socket())
-				assertThat(condition.isRequestMatching(request)).isTrue
-
-				val url = captor.firstValue
-				assertThat(url.scheme).isEqualTo("http")
-				assertThat(url.host).isEqualToIgnoringCase(socket.inetAddress.hostName)
-				assertThat(url.pathSize).isEqualTo(1)
-			}
-		}
-	}
-
 }
