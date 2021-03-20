a2d763c35d57c9017d6429f73201d7f3c50085b0
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2021-03-20 14:06:09 +0100

    Buildscript code cleanup

--- dispatcher/build.gradle.kts
+++ dispatcher/build.gradle.kts
@@ -14,9 +14,7 @@ dependencies {
     implementation("org.apache.commons:commons-text:1.9")
     implementation("pl.droidsonroids.yaml:snakeyaml:1.18-android")
     testImplementation("org.assertj:assertj-core:3.19.0")
-    testImplementation("com.nhaarman:mockito-kotlin:1.6.0") {
-        //exclude group: ("org.jetbrains.kotlin")
-    }
+    testImplementation("com.nhaarman:mockito-kotlin:1.6.0")
     testImplementation("org.mockito:mockito-core:3.8.0")
     testImplementation("junit:junit:4.13.1")
     testImplementation("nl.jqno.equalsverifier:equalsverifier:3.5.5")
