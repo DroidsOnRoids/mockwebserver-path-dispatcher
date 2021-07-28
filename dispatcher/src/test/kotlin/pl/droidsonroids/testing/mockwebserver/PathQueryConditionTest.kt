2d7c7cce3ac99c888655a2bad761c4b7977ec12a
Eduard Bosch <eduardbosch@citibox.com>
2021-07-28 16:34:05 +0200

    fix: Sort with path if they are equal

--- dispatcher/src/test/kotlin/pl/droidsonroids/testing/mockwebserver/PathQueryConditionTest.kt
+++ dispatcher/src/test/kotlin/pl/droidsonroids/testing/mockwebserver/PathQueryConditionTest.kt
@@ -113,4 +113,109 @@ class PathQueryConditionTest {
     fun `equals and hashCode match contract`() {
         EqualsVerifier.forClass(PathQueryCondition::class.java).verify()
     }
+
+    @Test
+    fun `compareTo should return 0 when conditions are equals without parameters`() {
+        assertThat(suffixPathQueryCondition.compareTo(suffixPathQueryCondition)).isEqualTo(0)
+    }
+
+    @Test
+    fun `compareTo should return 0 when conditions are equals with parameter name`() {
+        assertThat(parameterNamePathQueryCondition.compareTo(parameterNamePathQueryCondition))
+            .isEqualTo(0)
+    }
+
+    @Test
+    fun `compareTo should return 0 when conditions are equals with parameter value`() {
+        assertThat(parameterValuePathQueryCondition.compareTo(parameterValuePathQueryCondition))
+            .isEqualTo(0)
+    }
+
+    @Test
+    fun `compareTo should return a number lower than 0 when conditions are different with the same score and the first condition path is lower than the second`() {
+        val firstCondition = PathQueryConditionFactory()
+            .withPathSuffix("/abc")
+        val secondCondition = PathQueryConditionFactory()
+            .withPathSuffix("/cba")
+
+        assertThat(firstCondition.compareTo(secondCondition))
+            .isLessThan(-1)
+    }
+
+    @Test
+    fun `compareTo should return a number higher than 0 when conditions are different with the same score and the first condition path is higher than the second`() {
+        val firstCondition = PathQueryConditionFactory()
+            .withPathSuffix("/cba")
+        val secondCondition = PathQueryConditionFactory()
+            .withPathSuffix("/acb")
+
+        assertThat(firstCondition.compareTo(secondCondition))
+            .isGreaterThan(0)
+    }
+
+    @Test
+    fun `compareTo should return -1 when the first condition has a parameter name and the second one does not`() {
+        val firstCondition = PathQueryConditionFactory()
+            .withPathSuffixAndQueryParameter("/abc", PARAMETER_NAME)
+        val secondCondition = PathQueryConditionFactory()
+            .withPathSuffix("/abc")
+
+        assertThat(firstCondition.compareTo(secondCondition))
+            .isEqualTo(-1)
+    }
+
+    @Test
+    fun `compareTo should return -1 when the first condition has a parameter value and the second one does not`() {
+        val firstCondition = PathQueryConditionFactory()
+            .withPathSuffixAndQueryParameter("/abc", PARAMETER_NAME, PARAMETER_VALUE)
+        val secondCondition = PathQueryConditionFactory()
+            .withPathSuffix("/abc")
+
+        assertThat(firstCondition.compareTo(secondCondition))
+            .isEqualTo(-1)
+    }
+
+    @Test
+    fun `compareTo should return -1 when the first condition has a parameter value and the second one has a parameter name`() {
+        val firstCondition = PathQueryConditionFactory()
+            .withPathSuffixAndQueryParameter("/abc", PARAMETER_NAME, PARAMETER_VALUE)
+        val secondCondition = PathQueryConditionFactory()
+            .withPathSuffixAndQueryParameter("/abc", PARAMETER_NAME)
+
+        assertThat(firstCondition.compareTo(secondCondition))
+            .isEqualTo(-1)
+    }
+
+    @Test
+    fun `compareTo should return 1 when the first condition does not have a parameter and the second one has a parameter name`() {
+        val firstCondition = PathQueryConditionFactory()
+            .withPathSuffix("/abc")
+        val secondCondition = PathQueryConditionFactory()
+            .withPathSuffixAndQueryParameter("/abc", PARAMETER_NAME)
+
+        assertThat(firstCondition.compareTo(secondCondition))
+            .isEqualTo(1)
+    }
+
+    @Test
+    fun `compareTo should return 1 when the first condition has a parameter name and the second one has a parameter value`() {
+        val firstCondition = PathQueryConditionFactory()
+            .withPathSuffixAndQueryParameter("/abc", PARAMETER_NAME)
+        val secondCondition = PathQueryConditionFactory()
+            .withPathSuffixAndQueryParameter("/abc", PARAMETER_NAME, PARAMETER_VALUE)
+
+        assertThat(firstCondition.compareTo(secondCondition))
+            .isEqualTo(1)
+    }
+
+    @Test
+    fun `compareTo should return 1 when the first condition does not have a parameter and the second one has a parameter value`() {
+        val firstCondition = PathQueryConditionFactory()
+            .withPathSuffix("/abc")
+        val secondCondition = PathQueryConditionFactory()
+            .withPathSuffixAndQueryParameter("/abc", PARAMETER_NAME, PARAMETER_VALUE)
+
+        assertThat(firstCondition.compareTo(secondCondition))
+            .isEqualTo(1)
+    }
 }
