af9e609ff693d0eedf0932de98f3d5fcc69b1803
Wim Bervoets <wim.bervoets@joyn.be>
2021-09-30 12:32:05 +0200

    Update Dependencies (https://github.com/DroidsOnRoids/mockwebserver-path-dispatcher/issues/23)

--- dispatcher/build.gradle.kts
+++ dispatcher/build.gradle.kts
@@ -1,20 +1,20 @@
 plugins {
     jacoco
-    id("org.jetbrains.kotlin.jvm") version ("1.4.31")
-    id("org.jetbrains.dokka") version ("1.4.30")
-    id("com.vanniktech.maven.publish") version ("0.13.0")
+    id("org.jetbrains.kotlin.jvm") version ("1.5.31")
+    id("org.jetbrains.dokka") version ("1.5.30")
+    id("com.vanniktech.maven.publish") version ("0.18.0")
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.31")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
     implementation("com.squareup.okhttp3:mockwebserver:4.9.1")
     implementation("org.apache.commons:commons-text:1.9")
-    implementation("pl.droidsonroids.yaml:snakeyaml:1.18-android")
-    testImplementation("org.assertj:assertj-core:3.19.0")
+    implementation("org.yaml:snakeyaml:1.29:android")
+    testImplementation("org.assertj:assertj-core:3.21.0")
     testImplementation("com.nhaarman:mockito-kotlin:1.6.0")
-    testImplementation("org.mockito:mockito-core:3.8.0")
-    testImplementation("junit:junit:4.13.1")
-    testImplementation("nl.jqno.equalsverifier:equalsverifier:3.5.5")
+    testImplementation("org.mockito:mockito-core:3.12.4")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("nl.jqno.equalsverifier:equalsverifier:3.7.1")
 }
 
 jacoco {
@@ -23,9 +23,9 @@ jacoco {
 
 tasks.jacocoTestReport {
     reports {
-        xml.isEnabled = true
-        csv.isEnabled = false
-        html.isEnabled = true
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(true)
     }
 }
 
