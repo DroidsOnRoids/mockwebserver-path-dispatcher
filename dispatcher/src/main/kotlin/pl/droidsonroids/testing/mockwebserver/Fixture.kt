a1781e9ceafb21f812e4450e0848fc2cfa0daa49
Eduard Bosch Bertran <eduardboschbertran@gmail.com>
2025-04-23 15:08:11 +0200

    Update snakeyaml to non-android 2.4
    Since 1.30, the android version is not available anymore, so we should use the regular version.
    This solves compatibility issues with other libraries that uses snakeyaml as using the android
    version forces resolving that variant.

--- dispatcher/src/main/kotlin/pl/droidsonroids/testing/mockwebserver/Fixture.kt
+++ dispatcher/src/main/kotlin/pl/droidsonroids/testing/mockwebserver/Fixture.kt
@@ -5,17 +5,11 @@ import okio.Buffer
 internal class Fixture {
 
     var statusCode = 0
-        internal set
     var body: String? = null
-        internal set
     var bodyContent: BodyContent? = null
-        internal set
     var headers: List<String> = emptyList()
-        internal set
     var connectionFailure: Boolean = false
-        internal set
     var timeoutFailure: Boolean = false
-        internal set
 
     internal fun hasJsonBody() = body?.isPossibleJson() ?: false
 }
