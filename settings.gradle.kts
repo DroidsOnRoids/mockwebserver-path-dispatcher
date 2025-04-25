d6e644030c33d7ba972ccd43de603bd5386fab6e
Eduard Bosch Bertran <eduardboschbertran@gmail.com>
2025-04-25 09:02:17 +0200

    Migrate to version catalogs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,10 @@
 include(":dispatcher")
 project(":dispatcher").name = extra["POM_ARTIFACT_ID"] as String
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
